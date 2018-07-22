dae87a62c49fe8c1c1db55f2fcbf592de7278e3f
KalyanSam <samala.kalyankumar@gmail.com>
2018-07-22 15:52:58 -0400

    del

--- 13_dictnories.py
+++
@@ -1,8 +0,0 @@
-details = {'name':'Kalyan','age':26}
-print(details)
-z = details['age']
-print(z)
-
-# lis_details = {'k1':'kalyan','k2':'[a,b,c]',{'age':'26'}}
-# y = lis_details['age']
-# print(y)
