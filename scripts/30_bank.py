e1e5c6dd188c67df612065b5a1a2b0eff4bdee82
KalyanSam <samala.kalyankumar@gmail.com>
2018-08-13 22:42:54 -0400

    assignment

--- scripts/30_bank.py
+++ scripts/30_bank.py
@@ -6,14 +6,13 @@ class Account():
 
     def deposit(self):
         dep = int(input("Enter amount to be deposited "))
-        print(dep)
         self.balance = self.balance + dep
         print(f"Available balance is {self.balance}")
 
     def withdraw(self):
         wth = int(input("Enter amount to be withdrawn "))
         print(wth)
-        if self.balance > wth:
+        if self.balance >= wth:
             self.balance = self.balance - wth
             print(f"Available balance is {self.balance}")
         else:
